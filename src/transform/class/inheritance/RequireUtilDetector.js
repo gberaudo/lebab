6c9c2ac4354883bfc4615c4672ae520771c362fc
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2018-06-29 08:38:38 +0200

    Detect inherits() directly

--- src/transform/class/inheritance/RequireUtilDetector.js
+++ src/transform/class/inheritance/RequireUtilDetector.js
@@ -1,56 +1,20 @@
-import {find} from 'lodash/fp';
-import {isAstMatch, matchesLength} from '../../../utils/matchesAst';
-
 /**
- * Detects variable name imported from require("util")
+ * Detects direct calls to inherits()
  */
 export default class RequireUtilDetector {
   /**
-   * Detects: var <identifier> = require("util")
    *
    * @param {Object} node
-   * @return {Object} MemberExpression of <identifier>.inherits
+   * @return {Object} MemberExpression of inherits
    */
   detect(node) {
     if (node.type !== 'VariableDeclaration') {
       return;
     }
 
-    const declaration = find(dec => this.isRequireUtil(dec), node.declarations);
-    if (declaration) {
-      return {
-        type: 'MemberExpression',
-        computed: false,
-        object: {
-          type: 'Identifier',
-          name: declaration.id.name,
-        },
-        property: {
-          type: 'Identifier',
-          name: 'inherits'
-        }
-      };
-    }
-  }
-
-  // Matches: <id> = require("util")
-  isRequireUtil(dec) {
-    return isAstMatch(dec, {
-      type: 'VariableDeclarator',
-      id: {
-        type: 'Identifier',
-      },
-      init: {
-        type: 'CallExpression',
-        callee: {
-          type: 'Identifier',
-          name: 'require'
-        },
-        arguments: matchesLength([{
-          type: 'Literal',
-          value: 'util'
-        }])
-      }
-    });
+    return {
+      type: 'Identifier',
+      name: 'inherits'
+    };
   }
 }
